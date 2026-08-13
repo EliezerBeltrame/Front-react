74efef89d888e898e9c6d24ef1128199b6e222bb
ELIÉZER SANTOS BELTRAME <eliezer.beltrame@senaisp.edu.br>
2026-08-12 22:47:02 -0300

    Adicionando exercicio 2 - Controle de Qualidade

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';// fazem a tela atualizar automaticamente.
+import './App.css'; //Importa os estilos CSS
 
-function App() {
+
+// Componente do Botão de Defeito
+function BotaoDefeito({ nome, quantidade, onClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={onClick} style={{ margin: '5px', padding: '10px 15px' }}>
+      {nome}: {quantidade} defeitos
+    </button>
   );
 }
 
-export default App;
+export default function App() {
+  const [rachaduras, setRachaduras] = useState(0);// valor inicial de cada contador.
+  const [pinturas, setPinturas] = useState(0);
+  const [peca, setPeca] = useState(0);
+
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Controle de Qualidade</h1>
+      {/* Imagem de Indústria via Link (Garantido que funciona) */}
+      
+      <BotaoDefeito 
+        nome="Rachaduras" 
+        quantidade={rachaduras} 
+        onClick={() => setRachaduras(rachaduras + 1)} 
+      />
+
+      <BotaoDefeito 
+        nome="Pintura Descascada" 
+        quantidade={pinturas} 
+        onClick={() => setPinturas(pinturas + 1)} 
+      />
+      <BotaoDefeito 
+        nome="Peça trincada" 
+        quantidade={peca} 
+        onClick={() => setPeca(peca + 1)} 
+      />
+      <hr />
+      <h3>Total de Defeitos: {rachaduras + pinturas + peca}</h3>
+    </div>
+    //atualiza o total na tela imediatamente.
+  );
+}
